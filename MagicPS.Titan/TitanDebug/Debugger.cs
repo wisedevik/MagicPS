14c055be090610feeebd3eae909557097f57bf35
WiseDev <83840010+wisedevik@users.noreply.github.com>
2024-03-15 10:57:09 +0300

    fixed debugger error

---
+++ MagicPS.Titan/TitanDebug/Debugger.cs
@@ -0,0 +1,55 @@
+﻿using MagicPS.Titan.Exception;
+using System.Diagnostics;
+
+namespace MagicPS.Titan.Debug
+{
+    public static class Debugger
+    {
+        private static IDebuggerListener m_listener;
+
+        public static bool DoAssert(bool assertion, string assertionError)
+        {
+            if (!assertion)
+            {
+                m_listener.Error(assertionError);
+            }
+
+            return assertion;
+        }
+
+        public static void HudPrint(string log)
+        {
+            m_listener.HudPrint(log);
+        }
+
+        public static void Print(string log)
+        {
+            m_listener.Print(log);
+        }
+
+        public static void Warning(string log)
+        {
+            m_listener.Warning(log);
+        }
+
+        public static void Error(string log)
+        {
+            m_listener.Error(log);
+            throw new LogicException(log);
+        }
+
+        public static void SetListener(IDebuggerListener listener)
+        {
+            m_listener = listener;
+        }
+    }
+
+    public interface IDebuggerListener
+    {
+        void HudPrint(string message);
+        void Print(string message);
+        void Warning(string message);
+        void Error(string message);
+    }
+
+}
---
+++ MagicPS.Titan/TitanDebug/DebuggerListener.cs
@@ -0,0 +1,33 @@
+﻿namespace MagicPS.Titan.Debug;
+
+using Microsoft.Extensions.Logging;
+using System;
+
+public class DebuggerListener : IDebuggerListener
+{
+    private readonly ILogger _logger;
+
+    public DebuggerListener(ILoggerFactory loggerFactory)
+    {
+        _logger = loggerFactory.CreateLogger("Magic.Titan.Debug.Debugger");
+    }
+    public void HudPrint(string message)
+    {
+        _logger.LogInformation("{m}", message);
+    }
+
+    public void Print(string message)
+    {
+        _logger.LogInformation("{m}", message);
+    }
+
+    public void Warning(string message)
+    {
+        _logger.LogWarning("{m}", message);
+    }
+
+    public void Error(string message)
+    {
+        _logger.LogError("{m}", message);
+    }
+}
